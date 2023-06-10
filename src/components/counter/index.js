4589a3b33c208856858da8c5a630fa364ccc1418
Marco Tulio Costa <marcotscosta@gmail.com>
2023-06-09 23:03:21 -0300

    feature: creating set rest function

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -4,14 +4,29 @@ import { StyleSheet, Text, View, TouchableOpacity, Vibration } from 'react-nativ
 export default function Counter(props) {
 
   const timerRef = useRef();
-  const [rest, setRest] = useState(5);
-  const [timer, setTimer] = useState(rest);
+  const [rest, setRest] = useState();
+  const [timer, setTimer] = useState();
   const [status, setStatus] = useState();
 
 
+  useEffect(() => {
+    console.log(props);
+    setRest(props.restTime);
+    setTimer(props.restTime);
+  }, [props.restTime]);
+
+  const secondsToString = (timeInSeconds) => {
+    let minutes = Math.floor((timeInSeconds / 60));
+    console.log('Total Time:' + timeInSeconds);
+    console.log("Minutes: "+minutes);
+    let seconds = (timeInSeconds - (minutes * 60));
+    return minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+
+  };
+
   const updateTime = (start) => {
     let now = new Date();
-    aux = rest - ((now - start) / 1000);
+    aux = Math.ceil(rest - ((now - start) / 1000));
     if (aux >= 0) {
       setTimer(aux);
     } else {
@@ -20,28 +35,30 @@ export default function Counter(props) {
       Vibration.vibrate([200, 200, 200, 200], true)
       setTimeout(() => Vibration.cancel(), 7000)
     }
-  }
+  };
 
   const start = () => {
     setStatus('inRest')
+    setTimer(rest)
     let start = new Date();
     clearInterval(timerRef.current);
     timerRef.current = setInterval(() => updateTime(start), 500);
-  }
+  };
 
   const stop = () => {
     Vibration.cancel()
     setStatus('inTraining')
     clearInterval(timerRef.current);
-    setTimer(0);
+    setTimer(rest);
     console.log("Stop");
-  }
+  };
 
   return (
     <View style={styles.all}>
+
       <Text>{status}</Text>
       <View style={styles.timeContainer}>
-        <Text style={styles.timeText}> {timer}</Text>
+        <Text style={styles.timeText}> {secondsToString(timer)}</Text>
       </View>
       <View style={styles.buttonsContainer}>
         <TouchableOpacity onPress={start}>
--- src/screens/Timer/index.js
+++ src/screens/Timer/index.js
@@ -1,13 +1,36 @@
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, StyleSheet } from 'react-native';
 
-import Counter from '../../components/counter'; 
+import Counter from '../../components/counter';
 import TimePicker from '../../components/timePicker';
+import { useState } from 'react';
 
 export default function Timer() {
+
+
+  const [rest, setRest] = useState('120')
+
   return (
     <View>
-      
-      <Counter/>
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={setRest}
+          value={rest}
+          placeholder="Ex 120s"
+          keyboardType="numeric"
+        />
+      </View>
+      <Counter restTime={rest} />
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: '#D9D9D9',
+    borderRadius: 15,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+  }
+});
